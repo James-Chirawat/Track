fa4e260a01ad800da2745cfff3d5bbd124433d42
James-Chirawat <chirawat.emphan@enov8.com>
2025-09-29 14:17:21 +0700

    comment qrcode

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
   const navigation = [
     { name: 'แดชบอร์ด', href: '/', icon: 'ri-dashboard-line' },
     { name: 'แผนผังการผลิต', href: '/roadmap', icon: 'ri-route-line' },
-    { name: 'สแกน QR Code', href: '/scanner', icon: 'ri-qr-scan-line' },
+    // { name: 'สแกน QR Code', href: '/scanner', icon: 'ri-qr-scan-line' },
   ]
 
   return (
